63819efd63eacf7c48ed346ab392284dc109ce67
Praveennaik6 <praveennaik0814@gmail.com>
2025-05-15 21:03:43 +0530

    Home file update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,17 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
+        <div className='hero'>
+            <h1>Largest <br /> Crypto Marketplace</h1>
+            <p>Welcome to the world's largets cryptocurrency marketplace.
+                Sign up to explore more about cryptos.
+            </p>
+            <form>
+                <input type="text" placeholder='Search Crypto..' />
+                 <button type='submit'>Search</button>
+            </form>
+        </div>
       
     </div>
   )
