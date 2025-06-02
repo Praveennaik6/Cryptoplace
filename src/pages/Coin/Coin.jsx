cad96b5de4bd9699cb5c79f3c90a7dcb4d0e90f7
Praveennaik6 <praveennaik0814@gmail.com>
2025-06-02 22:39:00 +0530

    coin.jsx updated

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Coin.css'
 import { useParams } from 'react-router-dom'
 
 const Coin = () => {
 
   const {coinId}= useParams();
+  const [coinData,setCoinData]= useState();
+
+  const fetchCoinData = async () => {
+    const options={
+      method: 'GET',
+      headers:{accept:'application/json','x-cg-demo-api-key':'CG-91Na3gF37jLkMimFB9B4FtwP'}
+    };
+   fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+     
+    }
+    useEffect(() => {
+      fetchCoinData();
+
+
+    },[])
+    
   return (
     <div>
       
